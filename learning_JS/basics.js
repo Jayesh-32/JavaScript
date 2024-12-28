18f4ead1174fd121d3d72686438c76e282fbf539
Jayesh-32 <70850443+Jayesh-32@users.noreply.github.com>
2024-12-28 14:34:49 +0000

    Data type basics

--- learning_JS/basics.js
+++ learning_JS/basics.js
@@ -1,1 +1,26 @@
-console.log("Hello world");
+// console.log("Hello world");
+
+let num=89764565464654n;
+
+// console.log(typeof num);
+
+let name=["Jayu", "sam", "vivek"];
+
+// console.log(typeof name);
+ 
+const printname=function(){
+    console.log("Hello world");
+}
+
+const prof={
+    "id":123,
+    "name":"Jayesh"
+}
+// console.log(typeof prof);
+
+ /* primitive store in stack and non primitive store in heap */
+
+const prof2=prof;
+prof2.name="Jayesh Nakashe";
+
+console.log(prof);
