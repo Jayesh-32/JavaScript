d7ab0b597c698e74ba2bbc926ef6be0c1aba0eb1
Jayesh-32 <70850443+Jayesh-32@users.noreply.github.com>
2024-12-28 16:24:50 +0000

    Strings and umber dattatype and math lib

---
+++ learning_JS/number_math.js
@@ -0,0 +1,17 @@
+let score=54646545.4656;
+
+// console.log(score.toFixed(2));
+// console.log(score.toLocaleString('en-IN'));
+
+let dice=Math.floor(Math.random()*6+1);
+console.log(dice);
+
+let num=-6;
+console.log(Math.abs(-6));
+console.log(Math.round(56.23));
+
+let min=2;
+let max=10;
+
+let avg=Math.floor((Math.random()*(max-min+1)+min));
+console.log(avg);
---
+++ learning_JS/strings.js
@@ -0,0 +1,21 @@
+let name="  Jayesh  ";
+let age=22;
+let url="https://google.com";
+let address="Vasai";
+
+let vaillage="Satara nagpur nashik";
+
+// console.log(`Hello everyone I am ${name} from ${address} and I am ${age} years old.`);
+
+// console.log(name.length);
+// console.log(name.toUpperCase());
+
+// console.log(name.charAt(5));
+// console.log(name.indexOf('n'));
+// console.log(name.trimStart());
+// console.log(url.replace('google','yahoo'));
+// console.log(vaillage.split(' '));
+console.log(vaillage.substring(0,5));
+console.log(vaillage.slice(-19));
+
+
