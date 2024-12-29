30ca05a93e8e6fc357ec2ed8ee0c485f6a27c279
Jayesh Nakashe <70850443+Jayesh-32@users.noreply.github.com>
2024-12-29 11:30:33 +0000

    Array basics

---
+++ learning_JS/array2.js
@@ -0,0 +1,26 @@
+let marvelheroes=['thor', 'flash', 'ironman'];
+let dc_heroes=["bheem","superman","batman"];
+
+// marvelheroes.push(dc_heroes);
+// console.log(marvelheroes);
+
+let heroes=marvelheroes.concat(dc_heroes);
+// console.log(heroes);
+
+let newheroes=[...marvelheroes,...dc_heroes];
+// console.log(newheroes);
+
+let arr=[1, 2, 3, [4, 5], 6, [[7,8],9]]
+
+let newarr=arr.flat(Infinity);
+console.log(newarr);
+
+let name="Jayesh";
+let age=22;
+let id=46546;
+
+let val=Array.of(name, age, id);
+
+console.log(Array.isArray(val));
+
+console.log(Array.from(null));
