44b08ee41730db46876ee087e88c89b81fd72d9d
Jayesh Nakashe <70850443+Jayesh-32@users.noreply.github.com>
2024-12-29 05:10:26 +0000

    Date and time

---
+++ learning_JS/date.js
@@ -0,0 +1,21 @@
+let dt=new Date();
+
+// console.log(dt.toString());
+
+// console.log(dt.toLocaleString());
+// console.log(dt.toJSON());
+
+let date=new Date(2024,0,25);
+
+// console.log(date.toDateString());
+console.log(Date.now());
+
+console.log(date.getTime());
+
+
+
+let dt2=new Date();
+
+console.log(dt2.getFullYear());
+console.log(dt2.getDay());
+console.log(dt2.now())
