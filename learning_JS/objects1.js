9fa55a7765fd29a1d0fa700cd16416cc29c57364
Jayesh Nakashe <70850443+Jayesh-32@users.noreply.github.com>
2024-12-29 15:03:10 +0000

    Objects more

--- learning_JS/objects1.js
+++ learning_JS/objects1.js
@@ -23,5 +23,32 @@ user.func1=function(){
 // user.name="Jayesh Nakashe";
 // console.log(user.email);
 // console.log(user['Aadhar No']);
-console.log(user);
-console.log(user.func1());
+// 
+
+
+let new1={
+    1 :'a',2:'b',3:'c',4:'d'
+}
+let new2={
+    5 :'e',6:'f',7:'g',8:'h'
+}
+
+// let new3={new1,new2};
+
+let new3={...new1,...new2};
+
+// console.log(new3);
+
+let userprof={
+    name:"Jayesh",
+    age:22,
+    "Aadhar No":'2424 2424 2424',
+    email:"jayesh@gmail.test",
+    [sym]:'testing',
+    lastLogin:[101012, 1465464]
+}
+
+console.log(Object.keys(userprof));
+console.log(Object.values(userprof));
+console.log(Object.entries(userprof));
+
