8ee5dbc176150b66440bedb0a924d53b4b0038ff
Jayesh Nakashe <70850443+Jayesh-32@users.noreply.github.com>
2024-12-29 06:02:10 +0000

    array basics

---
+++ learning_JS/array1.js
@@ -0,0 +1,29 @@
+let name=["jayu", "vivek","pranay"];
+let age=[22,24,21];
+
+// console.log(name[0]);
+
+name.push("ankit");
+// console.log(name);
+
+// name.unshift("sam");
+// console.log(name);
+
+// name.pop();
+name.shift();
+// console.log(name);
+
+let nm=name.join();
+
+// console.log(nm);
+
+
+let arr1=[11, 22, 33, 44 , 55];
+console.log(arr1);
+let new1=arr1.slice(1, 4);
+console.log(new1);
+console.log(arr1);
+let new2 =arr1.splice(2,3,66);
+console.log(new2);
+console.log(arr1);
+
